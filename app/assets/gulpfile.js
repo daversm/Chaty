1fdd0a2fc6bd26032c3bb123786093ce768992d4
Daver <daversm@gmail.com>
2016-04-09 16:32:26 -0700

    outline of main index.html page

--- app/assets/gulpfile.js
+++ app/assets/gulpfile.js
@@ -4,19 +4,20 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var mode = 'development';
 
 gulp.task('apply-prod-environment', function() {
     process.stdout.write("Setting NODE_ENV to 'production'" + "\n");
-    process.env.NODE_ENV = 'production';
+    process.env.NODE_ENV = mode;
     if (process.env.NODE_ENV != 'production') {
-        throw new Error("Failed to set NODE_ENV to production!!!!");
+        process.stdout.write("Failed to set NODE_ENV to production!!!!");
     } else {
         process.stdout.write("Successfully set NODE_ENV to production" + "\n");
     }
 });
 
 gulp.task('build', function () {
-    return browserify({ entries: './app/assets/main.jsx', extensions: ['.jsx'], debug: true }).transform(babelify).bundle().pipe(source('mainBundle.js')).pipe(gulp.dest('./app/public/js'));
+    return browserify({ entries: './main.jsx', extensions: ['.jsx'], debug: true }).transform(babelify).bundle().pipe(source('mainBundle.js')).pipe(gulp.dest('../public/js'));
 });
 
 gulp.task('watch', ['build'], function () {
--- app/assets/main.jsx
+++ app/assets/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+
 var Chaty = React.createClass({
   getInitialState: function(){
     return({currentChatRoom:'NONE', userName:''});
   },
   render:function(){
     return (
-      <div>
+      <div className="chatBox">
         <div className="chatRooms"></ div>
         <div className="typingBox"></ div>
       </ div>
@@ -15,7 +16,7 @@ var Chaty = React.createClass({
   }
 });
 
-ReactDOM.render(
+render(
 <Chaty />,
 document.getElementById('chatyDiv')
 );
