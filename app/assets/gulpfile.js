fa39c50a4075d4d725a9c8f5ba7ab735e289200d
Daver <daversm@gmail.com>
2016-04-11 20:12:40 -0700

    set React to production mode

--- app/assets/gulpfile.js
+++ app/assets/gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
-var mode = 'development';
+var mode = 'production';
 
 gulp.task('apply-prod-environment', function() {
     process.stdout.write("Setting NODE_ENV to 'production'" + "\n");
